ad041d3e858630c6a47dc86c307bb1c33ac150a1
XcellityTechn <xcellitytechnologies@gmail.com>
2021-04-28 13:31:47 +0530

    password

--- sai_aff/membersignup/form.py
+++ sai_aff/membersignup/form.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django import forms
 
@@ -29,3 +29,20 @@ class EditProfileForm(UserChangeForm):
     class Meta:
         model = User
         fields = ('username', 'first_name','last_name','email','password','last_login','date_joined')
+
+class Passwordchangingform(PasswordChangeForm):
+    old_password = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'class': "form-control"}))
+    new_password1 = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'class': "form-control"}))
+    new_password2 = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'class': "form-control"}))
+
+    class Meta:
+        model = User
+        fields = ('old_password', 'new_password1','new_password2')
+
+class LoginAuthForm(AuthenticationForm):
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': "form-control",'placeholder': 'Username'}))
+    password = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'class': "form-control",'placeholder':'Password'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
--- sai_aff/membersignup/views.py
+++ sai_aff/membersignup/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm, AuthenticationForm
 from django.urls import reverse_lazy
-from .form import SignUpForm, EditProfileForm
+from .form import SignUpForm, EditProfileForm, Passwordchangingform,LoginAuthForm
 from django.contrib.auth.views import PasswordChangeView
 
 # Create your views here.
@@ -21,8 +21,12 @@ class UserEditview(generic.UpdateView):
         return self.request.user
 
 class PasswordChangeView(PasswordChangeView):
-    form_class = PasswordChangeForm
+    form_class = Passwordchangingform
     template_name='registration/change_password.html'
     success_url = reverse_lazy('post')
 
 
+#class LoginAuthview(generic.FormView):
+#    form_class = LoginAuthForm
+#    template_name='registration/login.html'
+#    success_url = reverse_lazy('post')
