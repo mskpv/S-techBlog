e190f3e5c28ad91a913fdcf8c718230a5f269d24
XcellityTechn <xcellitytechnologies@gmail.com>
2021-05-25 13:15:50 +0530

    new-add

--- sai_aff/mskpv/urls.py
+++ sai_aff/mskpv/urls.py
@@ -7,7 +7,7 @@ from .views import post, article, Addpost_view, Updatepost_view, Deletepost_view
 urlpatterns = [
     path('', views.index, name='index'),
     path('2/', views.index_2, name='2'),
-    path('post/', post.as_view(), name='post' ),
+    path('post', post.as_view(), name='post' ),
     path('article/<int:pk>/', article.as_view(), name='article-details' ),
     path('addpost/',Addpost_view.as_view(), name = 'addpost'),
     path('article/edit/<int:pk>/', Updatepost_view.as_view(), name='Update_post' ),
