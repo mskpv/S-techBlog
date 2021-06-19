ca7acefae607777ca59a179fbf0092386e453799
XcellityTechn <xcellitytechnologies@gmail.com>
2021-06-20 00:04:56 +0530

    security_key

--- sai_aff/sai_aff/settings.py
+++ sai_aff/sai_aff/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c82au+o7=1ztrc4#u0(x)w=(#2w1$zvrg211h+=-ay1pl1l3+u'
+with open(BASE_DIR /'secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
