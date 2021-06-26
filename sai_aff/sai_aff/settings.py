f78e430fc77013de5894d8ff93d975330da3c9b4
XcellityTechn <xcellitytechnologies@gmail.com>
2021-06-26 23:57:49 +0530

    auth_reset

--- sai_aff/sai_aff/settings.py
+++ sai_aff/sai_aff/settings.py
@@ -101,6 +101,8 @@ DATABASES = {
     }
 }
 
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
