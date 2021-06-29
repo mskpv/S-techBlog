4d54f45451e079534fc29a5b3b6a1172839ae3bc
XcellityTechn <xcellitytechnologies@gmail.com>
2021-06-29 14:45:02 +0530

    overlap

--- sai_aff/membersignup/views.py
+++ sai_aff/membersignup/views.py
@@ -7,6 +7,19 @@ from .form import SignUpForm, EditProfileForm, Passwordchangingform, LoginAuthFo
 from django.contrib.auth.views import PasswordChangeView, PasswordResetConfirmView, PasswordResetView
 from mskpv.models import Profile, Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth.models import User
+from django.utils.http import urlsafe_base64_encode
+from django.shortcuts import render, redirect
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.db.models.query_utils import Q
+import email, smtplib, ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 
 # Create your views here.
 class CreateProfilepageView(CreateView):
@@ -73,9 +86,51 @@ class Passwdresetview(PasswordResetConfirmView):
     form_class = Passwdresetform
     template_name='registration/passwd_reset_form.html'
 
-class passwdemailview(PasswordResetView):
-    form_class = passwdemailform
-    template_name = 'registration/reset_passwd.html'
+#class passwdemailview(PasswordResetView):
+#    form_class = passwdemailform
+#    template_name = 'registration/reset_passwd.html'
+
+def password_reset_request(request):
+ if request.method == "POST":
+    password_reset_form = passwdemailform(request.POST)
+    if password_reset_form.is_valid():
+        data = password_reset_form.cleaned_data['email']
+        associated_users = User.objects.filter(Q(email=data))
+        if associated_users.exists():
+            for user in associated_users:
+                subject = "Password Reset Requested"
+                email_template_name = "registration/password_reset_email.txt"
+                c = {
+                "email":user.email,
+                'domain':'127.0.0.1:8000',
+                'site_name': 'Website',
+                "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                "user": user,
+                'token': default_token_generator.make_token(user),
+                'protocol': 'http',
+                }
+                email = render_to_string(email_template_name, c)
+                subject = "Your Password Reset"
+                body = email
+                sender_email = "support@stechmpv.xyz"
+                receiver_email = "saitech.in.tamil@gmail.com"
+                password = "$@!tech.!n.T@m!l"
+
+                # Create a multipart message and set headers
+                message = MIMEMultipart();message["From"] = sender_email;message["To"] = receiver_email;message["Subject"] = subject
+                message.attach(MIMEText(body, "plain"))
+                text = message.as_string()
+                try:
+                    context = ssl.create_default_context()
+                    with smtplib.SMTP_SSL("mail.stechmpv.xyz", 465, context=context) as server:
+                        server.login(sender_email, password)
+                        server.sendmail(sender_email, receiver_email, text)
+                except:
+                    return HttpResponse('Invalid header found.')
+                return redirect ("/members/reset_password_sent/")
+ password_reset_form = passwdemailform()
+ return render(request=request, template_name="registration/reset_passwd.html", context={"form":password_reset_form})
+
 #class LoginAuthview(generic.FormView):
 #    form_class = LoginAuthForm
 #    template_name='registration/login.html'
