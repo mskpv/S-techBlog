d86ac84fefa3ea020c6b035a5fa6d23a7f6ca66f
XcellityTechn <xcellitytechnologies@gmail.com>
2021-07-07 12:41:46 +0530

    new

--- sai_aff/membersignup/admin.py
+++ sai_aff/membersignup/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Visitor_Infos
 
 # Register your models here.
+admin.site.register(Visitor_Infos)
--- sai_aff/membersignup/models.py
+++ sai_aff/membersignup/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Visitor_Infos(models.Model):
+    ip_address = models.GenericIPAddressField()
+    page_visited = models.TextField()
+    event_date = models.DateTimeField(auto_now= True)
